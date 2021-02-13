5f8d857de3b141a35eb8a267bcd24cfde30cb47b
SteveOtieno14 <jjmenya14@gmail.com>
2021-02-12 19:09:13 -0600

    added comments to body

---
+++ main.js
@@ -0,0 +1,5 @@
+ var table_properties= ["Official Name:", "Form of Government:", "Capital:", "Population:", "Official Language:", "Currency:" ]
+
+ var table_values = ["Republic of Kenya:", "Republiuc", "Nairobi", "45,010,056", "Swahili, English", "Kenyan Shillings"]
+
+ 
