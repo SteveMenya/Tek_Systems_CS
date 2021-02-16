06a11224e3240f7260a5fa28d8ae087ad6d13b92
SteveOtieno14 <jjmenya14@gmail.com>
2021-02-16 07:46:15 -0600

    added  style

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
- var table_properties= ["Official Name:", "Form of Government:", "Capital:", "Population:", "Official Language:", "Currency:" ]
 
- var table_values = ["Republic of Kenya:", "Republiuc", "Nairobi", "45,010,056", "Swahili, English", "Kenyan Shillings"]
 
- 
+const changeText = () => {
+    const p = document.querySelector('p');
+    p.textContent = "I changed because of an inline event handler.";
+    console.log(p)
+
+
+
+//  var table_properties= ["Official Name:", "Form of Government:", "Capital:", "Population:", "Official Language:", "Currency:" ]
+
+//  var table_values = ["Republic of Kenya:", "Republiuc", "Nairobi", "45,010,056", "Swahili, English", "Kenyan Shillings"]
+
+//  var table= document.querySelector("tbody");
+//  var row= document.createElement("tr");
+//  table.appendChild(row)
+// var td= document.createElement("td");
+// td.textContent= "hello test"
+// row.appendChild(td)
